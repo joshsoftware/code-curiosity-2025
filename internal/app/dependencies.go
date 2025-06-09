5c4e1e8f7d177add9338a2e8f33e10cb130f7396
saurabhkale77 <saurabh.kale@joshsoftware.com>
2025-06-09 15:53:47 +0530

    Configured in router.go, which handler should be called with authentication when get badge details of user API is called and added related dependencies.

--- internal/app/dependencies.go
+++ internal/app/dependencies.go
@@ -3,6 +3,7 @@ package app
 import (
 	"github.com/jmoiron/sqlx"
 	"github.com/joshsoftware/code-curiosity-2025/internal/app/auth"
+	"github.com/joshsoftware/code-curiosity-2025/internal/app/badge"
 	"github.com/joshsoftware/code-curiosity-2025/internal/app/user"
 	"github.com/joshsoftware/code-curiosity-2025/internal/config"
 	"github.com/joshsoftware/code-curiosity-2025/internal/repository"
@@ -13,23 +14,28 @@ type Dependencies struct {
 	UserService user.Service
 	AuthHandler auth.Handler
 	UserHandler user.Handler
+	BadgeHandler badge.BadgeHandler
 	AppCfg      config.AppConfig
 }
 
 func InitDependencies(db *sqlx.DB, appCfg config.AppConfig) Dependencies {
 	userRepository := repository.NewUserRepository(db)
+	badgeRepository := repository.NewBadgeRepository(db)
 
 	userService := user.NewService(userRepository)
 	authService := auth.NewService(userService, appCfg)
+	badgeService := badge.NewBadgeService(badgeRepository)
 
 	authHandler := auth.NewHandler(authService, appCfg)
 	userHandler := user.NewHandler(userService)
+	badgeHandler := badge.NewBadgeHandler(badgeService)
 
 	return Dependencies{
 		AuthService: authService,
 		UserService: userService,
 		AuthHandler: authHandler,
 		UserHandler: userHandler,
+		BadgeHandler: badgeHandler,
 		AppCfg:      appCfg,
 	}
 }
--- internal/app/router.go
+++ internal/app/router.go
@@ -20,5 +20,7 @@ func NewRouter(deps Dependencies) http.Handler {
 
 	router.HandleFunc("PATCH /api/v1/user/email", middleware.Authentication(deps.UserHandler.UpdateUserEmail, deps.AppCfg))
 
+	router.HandleFunc("GET /api/v1/user/badges", middleware.Authentication(deps.BadgeHandler.GetBadgeDetailsOfUser, deps.AppCfg))
+
 	return middleware.CorsMiddleware(router, deps.AppCfg)
 }
