165fec8d271389cd75ac9761e8fdf879c8bb0d99
VishakhaSainani <vishakhasainani26@gmail.com>
2025-06-30 12:27:11 +0530

    seperate and use one alter table per migrations

--- internal/db/migrations/1750328591_add_column_contributors_url.up.sql
+++ internal/db/migrations/1750328591_add_column_contributors_url.up.sql
@@ -1,5 +1,1 @@
-ALTER TABLE repositories ADD COLUMN contributors_url VARCHAR(255);
-
-UPDATE repositories SET contributors_url = '' WHERE contributors_url IS NULL;
-
-ALTER TABLE repositories ALTER COLUMN contributors_url SET NOT NULL;
+ALTER TABLE repositories ADD COLUMN contributors_url VARCHAR(255) DEFAULT '';
